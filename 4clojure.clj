ac07c7bba404b423dd3ab742da3f2354eee76235
kero_hirake <k16.h.white@gmail.com>
2021-12-02 10:23:47 +0900

    2

--- 4clojure.clj
+++ 4clojure.clj
@@ -29,9 +29,56 @@
 ;Write a function which returns the total number of elements in a sequence.
 (def __ #(reduce (fn [a b] (inc a)) 0 %))
 
-(= (__ '(1 2 3 3 1)) 5)
+(= (reduce (fn [a b] (inc a)) 0 '(1 2 3 3 1)) 5)
 (= (__ "Hello World") 11)
 (= (__ [[1 2] [3 4] [5 6]]) 3)
 (= (__ '(13)) 1)
 (= (__ '(:a :b :c)) 3)
+
+;Problem 23
+;Write a function which reverses a sequence.
+(def __ (fn [xs] (reduce #(cons %2 %1) [] xs)))
+
+(= (reduce #(cons %2 %1) [] [1 2 3 4 5]) [5 4 3 2 1])
+(= (__ (sorted-set 5 7 2 7)) '(7 5 2))
+(= (__ [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]])
+
+;Problem 24
+;rite a function which returns the sum of a sequence of numbers.
+(def __ #(reduce + 0 %))
+
+(= (reduce + 0 [1 2 3]) 6)
+(= (__ (list 0 -2 5 5)) 8)
+(= (__ #{4 2 1}) 7)
+(= (__ '(0 0 -1)) -1)
+(= (__ '(1 10 3)) 14)
+
+;Problem 25
+;Write a function which returns only the odd numbers from a sequence.
+(def __ #(filter odd? %))
+
+(= (filter odd? #{1 2 3 4 5}) '(1 3 5))
+(= (__ [4 2 1 6]) '(1))
+(= (__ [2 2 4 6]) '())
+(= (__ [1 1 1 3]) '(1 1 1 3))
+
+;Problem 26
+;Write a function which returns the first X fibonacci numbers.
+(def __ #(->> (iterate (fn [[a b]] [b (+ a b)]) [1 1])
+            (take %)
+            (map first)))
+
+(= (__ 3) '(1 1 2))
+(= (__ 6) '(1 1 2 3 5 8))
+(= (__ 8) '(1 1 2 3 5 8 13 21))
+
+;Problem 27
+;Write a function which returns true if the given sequence is a palindrome. Hint: "racecar" does not equal '(\r \a \c \e \c \a \r)
+(def __ #(= (seq %) (reverse %)))
+
+(false? (__ '(1 2 3 4 5)))
+(true? (__ "racecar"))
+(true? (__ [:foo :bar :foo]))
+(true? (__ '(1 1 3 3 1 1)))
+(false? (__ '(:a :b :c)))
 
